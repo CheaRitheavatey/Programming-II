cd0a28748e58b0ba256451810b6e8c226fc61994
vatey <ritheavateychea@gmail.com>
2025-03-31 17:00:12 +0200

    Update

--- week9.java
+++ week9.java
@@ -1,5 +1,5 @@
-import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
@@ -7,6 +7,12 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.text.CollationElementIterator;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 
 public class week9 {
     public static void fileInput() throws IOException{
@@ -57,7 +63,7 @@ public class week9 {
     }
 
     public static void path() throws IOException{
-        Path p = Paths.get("week0File.txt");
+        Path p = Paths.get("week9File.txt");
         System.out.println("p.toString(): " + p.toString());
 
         // string that can be open from a browser
@@ -97,12 +103,82 @@ public class week9 {
         } else {
             System.out.println("file already delete or file not found");
         }
+
+        // Files.copy(source,target) to copy one file to another
+        // Files.move(p3,Paths.get("../"))
+
+
+        // meta data
+        System.out.println(Files.size(p2));  // return 0 if file not exist else it return a byte/bit
+        System.out.println(Files.getOwner(p2));  // it get the owner of the file or the directory
+        System.out.println(Files.isDirectory(p2)); // get is it a directory or not
+        System.out.println(Files.isRegularFile(p)); // Tests whether a file is a regular file with opaque content.
+
+
+        // read the content of the file
+        System.out.println(Files.readAllLines(p2)); // return list where each line is an item
+        
+        // write to a file
+        Files.write(p2, Files.readAllLines(p2)); // so its like copying from one file to another as well
+        ArrayList<String> fruits = new ArrayList<>();
+        Collections.addAll(fruits, "Apple", "Banana");
+        Files.write(p2, fruits,StandardOpenOption.APPEND); // StandardOpenOption will have a lot of method like append so its not ging to erase it
+        Files.write(p, fruits,StandardOpenOption.CREATE, StandardOpenOption.APPEND); // StandardOpenOption.CREATE = will create a file if it doesnt exist
+        // can add many option not just one
+
+    }
+
+    public static void example1(Path path) throws FileNotFoundException, IOException{
+        // create a method to count the number of the lines in a text file that its path was passed as an argument
+
+        // check if the file exist
+        
+        if (!Files.exists(path)) {
+           throw new FileNotFoundException();
+        }
+
+        // int count = 0;
+        System.out.println(Files.readAllLines(path).size());  // return the number of line
+        // System.out.println();
+        // for (int i = 0; i<Files.readAllLines(path).toArray().length; i++) {
+        //     count += i;
+        // }
+        // System.out.println("Count: " + count);
+
+       
+    }
+
+    // create a method that check a file to see if a word was mention inside a file and print the first match
+    public static void example2(Path path, String word) throws IOException, FileNotFoundException {
+        if (!Files.exists(path)) {
+            throw new FileNotFoundException();
+        }
+
+        // System.out.println(Files.readString(path));
+        List<String> f = Files.readAllLines(path);
+        System.out.println(f.toString());
+
+        int count = 0;
+        
+        for (String i: f) {
+                if (i.toLowerCase().contains(word)) {
+                    count++;
+                    System.out.println(word);
+                    // return true;
+                }
+            
+        }
+        // return false;
+        System.out.println(count);
+
+
     }
     public static void main(String[] args) throws IOException{
         // fileInput();
         // unichar();
-        path();
-
+        // path();
+        // example1(Paths.get("week8File.txt"));
+        example2(Paths.get("week8File.txt"), "value");
         
     }
 }
