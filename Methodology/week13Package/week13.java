e65235e61c3cabb10626691dd3cfdc8a8fa1557a
vatey <ritheavateychea@gmail.com>
2025-05-09 11:29:30 +0200

    Update week13.java

--- Methodology/week13Package/week13.java
+++ Methodology/week13Package/week13.java
@@ -34,6 +34,17 @@ public class week13 {
         previewClass(2.3);
 
         previewClass("able", 1222);
+
+        addOne(12.22);
+
+        Integer[] li = {1,2,4,5};
+        Character[] ch = {'a', 'b', 'c'};
+        printArray(li);
+
+        System.out.println("Compare " + compare(li, 1));
+        System.out.println("Compare " + compare(ch, '2'));
+
+        averager(li);
         
     }
     static public <U> void previewClass(U user) {
@@ -42,6 +53,46 @@ public class week13 {
     static public <U,S> void previewClass(U user, S user2) {
         System.out.println("U: "+ user.getClass().getName());
         System.out.println("S: "+ user2.getClass().getName());
+    }
+
+    // extens anything even if its an interface (not implement but extends for this one)
+    static public <U extends Number> void addOne(U number) {
+        System.out.println("U: " + number.getClass().getName());
+        System.out.println("addOne: " + (number.doubleValue() + 1));
+        
+    }
+
+    // print the number of items in an array that are greater than a parameter
+    static public <T extends Comparable> int compare(T[] arrayList, T para) {
+        int count = 0;
+        for (T e: arrayList) {
+            // >, < only compatible with primitive types so we need to implement comparable interface to compare the 2 and 
+            // it return negative, 0 or poistive value depend on whether e was greater than para or not
+            if (e.compareTo(para) > 0) {
+                count++;
+            }
+        }
+        return count;
+
+                
+    }
+
+    // create a method that can process any kind of array and print the element
+    static public <T> void printArray(T[] arr) {
+        for (T t: arr) {
+            System.out.println(t);
+        }
+    }
+
+    // pass any kind of array and return the average
+    public static <T extends Number> void averager(T[] arr) {
+        double tempt = 0;
+        for (T t: arr) {
+            tempt += t.doubleValue();  
+                 
+        }
+
+        System.out.println(tempt/arr.length);
     }
     
 }
