91570df4e19390d673bb7a950084bbddb0d43c8c
vatey <ritheavateychea@gmail.com>
2025-05-18 15:55:29 +0200

    Update

---
+++ GUI/groceryitem.java
@@ -0,0 +1,24 @@
+package GUI;
+
+public class groceryitem {
+    // data field
+    private String item;
+    private int quality;
+
+    groceryitem(String item, int quality) {
+        this.item = item;
+        this.quality = quality;
+    }
+
+    // getter
+    public String getItems() {
+        return this.item;
+    }
+    public int getquality() {
+        return this.quality;
+    }
+    public String toString() {
+        return this.item + " ( Quanity: " + this.quality + " )";
+    }
+    
+}
