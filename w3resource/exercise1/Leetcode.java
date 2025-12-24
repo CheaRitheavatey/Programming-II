fcd4fb1e3a96a5ae4a6d2086625ead1d461a154a
Chea Ritheavatey <2023447chea@aupp.edu.kh>
2025-12-24 21:19:15 +0100

    Update

--- w3resource/exercise1/Leetcode.java
+++ w3resource/exercise1/Leetcode.java
@@ -1,11 +1,50 @@
 package w3resource.exercise1;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Stack;
 
 public class Leetcode {
     public static void main(String[] args) {
         String s = "(){}}{";
-        System.out.println(isValid(s));
+//        System.out.println(isValid(s));
+
+        int[][] interval = {{1,3},{2,6},{8,10},{15,18}};
+//        interval[1] = new int[]{1, 2};
+
+//        System.out.println(Arrays.deepToString(interval));
+        System.out.println(merge(interval));
+    }
+
+    public static int[][] merge(int[][] interval) {
+        int[][] result = new int[interval.length][2];
+
+        for (int i = 0 ; i< interval.length - 1; i++) {
+            for (int j = 0; j < interval.length - 1 ; j++) {
+                if (interval[i][j] < interval[i+1][j]) {
+                    if (interval[i+1][j] - interval[i][j+1] <= 0) {
+                        result[i] = new int[]{interval[i][j], interval[i+1][j+1]};
+                        System.out.println(Arrays.deepToString(result));
+                    } else {
+                        result[i] = new int[]{interval[i][j], interval[i][j+1]};
+                        System.out.println(Arrays.deepToString(result));
+                    }
+
+                } else {
+                    if (interval[i][j] - interval[i+1][j+1] <= 0) {
+                        result[i] = new int[]{interval[i+1][j], interval[i][j+1]};
+                        System.out.println(Arrays.deepToString(result));
+                    } else {
+                        result[i] = new int[]{interval[i][j], interval[i][j+1]};
+                        System.out.println(Arrays.deepToString(result));
+                    }
+                }
+
+            }
+
+        }
+        System.out.println("result " + Arrays.deepToString(result));
+        return result;
     }
     
     public static boolean isValid(String s) {
